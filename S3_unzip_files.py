b69f6b490814fb67e969c4de6987d088b495a12b
mohamed1311990 <mohamed.salem.say@gmail.com>
2021-03-31 00:33:54 +0200

    Update S3_unzip_files.py

--- S3_unzip_files.py
+++ S3_unzip_files.py
@@ -1,31 +1,27 @@
-from botocore.exceptions import ClientError
 import logging
 import boto3
 import os 
 import zipfile
 
-# download S3 bucket
-BUCKET_NAME = 'mohamedsalembuket' # replace with your bucket name
-KEY = 'test.zip' # replace with your object key
-mys3 = boto3.resource('s3')
-try:
-    mys3.Bucket('mohamedsalembuket').download_file(KEY,'test.zip')
-except botocore.exceptions.ClientError as e:
-    if e.response['Error']['Code'] == "404":
-        print("The object does not exist.")
-    else:
-        raise
+def myfunction(event,context):
+  BUCKET_NAME = 'mohamedsalembuket'  # replace with your bucket name
+  KEY = 'test.zip'                   # replace with your object key
+  downloaded_zip = '/tmp/downloaded.zip'   #downloaded zip file path
+  extracted_files_path = '/tmp/work'       #extracted files location
 
-#extract S3 bucket files
-my_path = '/home/mo/Desktop/test/work'
-my_file = "/home/mo/Desktop/test/test.zip"
-with zipfile.ZipFile(my_file, 'r') as zip_ref:
-    zip_ref.extractall(my_path)
+  # download S3 bucket
+  mys3 = boto3.resource('s3')
+  # os.chdir('/tmp')
+  mys3.Bucket('mohamedsalembuket').download_file(KEY,downloaded_zip)
 
+  #extract S3 bucket files
+  with zipfile.ZipFile(downloaded_zip, 'r') as zip_ref:
+      zip_ref.extractall(extracted_files_path)
 
-#upload S3 files
-uploadeds3 = boto3.client('s3')
-for local_file in os.listdir(my_path):
-   os.chdir(my_path)
-   with open(local_file, 'rb') as f:
-      uploadeds3.upload_fileobj(f,BUCKET_NAME,local_file)
+  #upload extracted S3 files
+  uploadeds3 = boto3.client('s3')
+  for local_file in os.listdir(extracted_files_path):
+    os.chdir(extracted_files_path)
+    with open(local_file, 'rb') as f:
+      uploadeds3.upload_fileobj(f,BUCKET_NAME,local_file) 
+
